9e8fb57a29c6ac536468ea8690b92897eb644615
Utsab Adhikari <48797078+autsav@users.noreply.github.com>
2024-03-13 01:01:07 +0000

    router setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
+import Home from "./components/Home";
 import Profile from "./components/Profile"
 import Movie from "./components/Movie"
+import { BrowserRouter as Router, Routes, Route, Switch, Navigate } from 'react-router-dom'
+import Contact from "./components/Contact";
 
-export default function Gallery(){
+export default function App(){
   return(
-    <section>
-        <h1>
-          Amazing Scientist          
-        </h1>
-        <Profile />
-        <Movie />
+    <Router>
+          <Routes>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/contact' element={<Contact />} exact />
+          <Route path='/movie' element={<Movie />} exact />
+          <Route path='/profile' element={<Profile />} exact />
 
-    </section>
-  )
-}
+      
+          </Routes>        
+    </Router>
+  
+  );
+} 
