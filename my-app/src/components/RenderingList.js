c1227307d22f1ba124f8618e8440c3be79f26128
Utsab Adhikari <48797078+autsav@users.noreply.github.com>
2024-03-18 08:57:55 +0000

    Rendering List

--- my-app/src/components/RenderingList.js
+++ my-app/src/components/RenderingList.js
@@ -1,26 +1,103 @@
 import React from 'react'
+import { getImageUrl } from '../utils/utils';
 
-const people = [
-    'Creola Katherine Johnson: mathematician',
-    'Mario José Molina-Pasquel Henríquez: chemist',
-    'Mohammad Abdus Salam: physicist',
-    'Percy Lavon Julian: chemist',
-    'Subrahmanyan Chandrasekhar: astrophysicist'
-  ];
- function List() {
-    const ListItem = people.map(person => 
-        <li>{person}</li>
-    );
-    return(<ul>{ListItem}</ul>
+export const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'MK3eW3A'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'mynHUSa'
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageId: 'bE7W1ji'
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageId: 'IOjWm71'
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageId: 'lrWQx8l'
+}];
+
+function ListingAll(){
+    const listItems = people.map(person => {
+      return(
+        <li key={person.id}>
+          <img
+              src={getImageUrl(person.imageId)}
+              alt={person.name}
+           />
+           <p>
+            <b>{person.name}</b>
+            {' ' + person.profession + ' '}known for {person.accomplishment}
+           </p>
+
+        </li>
+      )
+    })
+    return(
+      <ul>{listItems}</ul>
     )
- }  
+
+}
+
+function List() {
+
+  const chemists = people.filter(person =>{ 
+    return( 
+            person.profession === 'chemist'
+          )
+        }
+      );
+
+  const listItems = chemists.map(person =>{
+    return (<>
+            <li key={person.id}>
+              <img
+                src={getImageUrl(person.imageId)}
+                alt={person.name}
+              />
+              <p>
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+              </p>
+            </li>
+
+    </>
+             
+            )
+    } );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
 
 function RenderingList() {
 
   return (
     <>
         <div>RenderingList</div>
+        <h1>Filtering the data </h1>
+        <h2>Only the chemist</h2>
         <List />  
+        <h1>Rendering all the data</h1>
+        <ListingAll />
     </>
    
   )
