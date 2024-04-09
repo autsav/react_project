0fca579c3cdec00b4a9524068b9401361095d6fe
Utsab Adhikari <48797078+autsav@users.noreply.github.com>
2024-04-09 15:55:45 +0100

    Update UpdatingArrayInState.js
    Added Shopping Cart

--- my-app/src/components/UpdatingArrayInState.js
+++ my-app/src/components/UpdatingArrayInState.js
@@ -342,7 +342,59 @@ function ItemList2({ artworks, onToggle }) {
   );
 }
 //-------------------------End of Bucket list-------------------------
+  //-------start of shopping cart-------/
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+     setProducts(products.map(product =>{
+       if (product.id === productId){
+         return { ...product,
+                count: product.count+1}
+       }else{
+         return product
+       }
+     }))
   
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
+//----------end of shopping cart 
 
 function UpdatingArrayInState() {
   return (
@@ -368,6 +420,10 @@ function UpdatingArrayInState() {
     <br></br>
     <h2> BucketList </h2>
     <BucketList2 />
+
+    <br></br>
+    <h2> shopping cart </h2>
+      <ShoppingCart />
 
     </div>
 
