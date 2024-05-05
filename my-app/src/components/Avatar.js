de1679233f2e50faa584474f42f93286104d163e
Utsab Adhikari <48797078+autsav@users.noreply.github.com>
2024-05-05 23:42:30 +0100

    Update Avatar.js

--- my-app/src/components/Avatar.js
+++ my-app/src/components/Avatar.js
@@ -1,53 +1,47 @@
-import React from 'react'
-//using curly bracket
+import React from 'react';
+
 const today = new Date().getDate();
 
 function formatDate(date) {
     return new Intl.DateTimeFormat(
-      'en-UK',
-      { weekday: 'long' }
+        'en-UK',
+        { weekday: 'long' }
     ).format(date);
-  }
-
-function dateSuffix(date){
-     if ( date >= 11 && date <= 13){
-        return 'th'
-     }
-       
-        switch(date % 10 ){
-            case 1:
-                return 'st'
-            case  2:
-                return 'nd'
-            case  3:
-                return 'rd'
-            default:
-                return 'th' 
-            
-        }            
 }
 
-function Avatar({person, size = 100 }) {
-    const source = "https://m.media-amazon.com/images/M/"
-    const description = "Avatar image"
-  return (
-    <div>
-        <h1>AVATAR</h1>
-        <img
-            className='avatar'
-            src={ source + person.imageId + '.jpg' }
-            alt={ person.name }
-            width={size.width}
-            height={size.height}
+function dateSuffix(date) {
+    if (date >= 11 && date <= 13) {
+        return 'th';
+    }
 
-        />
-        <br />
-        Going to watch this move on { today }{ dateSuffix(today) } , { formatDate(today) }
-        {/* Going to watch this move on { today }, { formatDate(today) } */}
-
-    </div>
-
-  )
+    switch (date % 10) {
+        case 1:
+            return 'st';
+        case 2:
+            return 'nd';
+        case 3:
+            return 'rd';
+        default:
+            return 'th';
+    }
 }
 
-export default Avatar   
+function Avatar({ person, size = 100 }) {
+    const source = "https://m.media-amazon.com/images/M/";
+    return (
+        <div>
+            <h1>AVATAR</h1>
+            <img
+                className='avatar'
+                src={source + person.imageId + '.jpg'}
+                alt={person.name}
+                width={size.width}
+                height={size.height}
+            />
+            <br />
+            Going to watch this move on {today}{dateSuffix(today)}, {formatDate(today)}
+        </div>
+    );
+}
+
+export default Avatar;
